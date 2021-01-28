58820986b4797aca275ed3599b6aef44f9de8c83
chaimjfishman <chaim.fishman2@gmail.com>
2021-01-27 21:17:52 -0500

    implmented server routes

--- server/routes.js
+++ server/routes.js
@@ -6,11 +6,28 @@ var db = require('./db.js');
 /* -------------------------------------------------- */
 
 async function getMembers(req, res) {
-
+  var members = await db.getAllMembers();
+  res.json(members)
 };
 
 async function addShift(req, res) {
+  console.log('addShift called')
+  var userId = req.params.userid;
+  var startTime = new Date(req.params.start);
+  var endTime = new Date(req.params.end);
+  var shiftType = req.params.type;
 
+  let shift = {
+    userId: userId,
+    startTime: startTime,
+    endTime: endTime,
+    shiftType: shiftType
+  }
+  // //TODO: add error handling
+  await db.addShiftDocument(shift);
+  
+  console.log(shift)
+  res.sendStatus(200)
 };
 
 // The exported functions, which can be accessed in index.js.
