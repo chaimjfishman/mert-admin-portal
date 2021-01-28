c41ba28f4c9fa8430b4f98b3fb8c82bf99545072
chaimjfishman <chaim.fishman2@gmail.com>
2021-01-28 01:55:39 -0500

    added multiselect dropdown

--- client/src/components/PushNotificationSender.js
+++ client/src/components/PushNotificationSender.js
@@ -2,6 +2,10 @@ import React from 'react';
 import PageNavbar from './PageNavbar';
 import '../style/PushNotificationSender.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { Multiselect } from 'multiselect-react-dropdown';
+
+
+
 export default class PushNotificationSender extends React.Component {
 	constructor(props) {
 		super(props);
@@ -9,9 +13,15 @@ export default class PushNotificationSender extends React.Component {
 		// State maintained by this React component
 		this.state = {
             members: [],
+            options: [{name: 'd', id: 1},{name: 'a', id: 1},{name: 'b', id: 1},{name: 'c', id: 1},{name: 'Srigar', id: 1},{name: 'Sam', id: 2}]
         }
 
-        this.handleMemberChange = this.handleMemberChange.bind(this);
+        this.onSelect = this.onSelect.bind(this);
+        this.onRemove = this.onRemove.bind(this);
+        this.resetSelcted = this.resetSelcted.bind(this);
+        this.sendPushNotification = this.sendPushNotification.bind(this);        
+
+        this.multiselectRef = React.createRef();
     }
     
     componentDidMount() {
@@ -36,24 +46,52 @@ export default class PushNotificationSender extends React.Component {
           .catch(err => console.log(err))	// Print the error if there is one.
     }
 
-    handleMemberChange(e) {
-		this.setState({
-			selectedMember: e.target.value
-        }, () => console.log(`member changed to ${this.state.selectedMember}`));   
+    sendPushNotification() {
+        console.log(this.multiselectRef.current.getSelectedItems())
+    }
+
+    onSelect(selectedList, selectedItem) {
+        console.log('selected')
+    }
+    
+    onRemove(selectedList, removedItem) {
+        console.log('removed')
+    }
+
+    resetSelcted() {
+        this.multiselectRef.current.resetSelectedValues()
     }
 	
 	render() {
 		return (
 			<div className="PushNotificationSender">
                 <PageNavbar active="notifications" />
-                
-                <button 
-                    className="submit-btn" 
-                    id="decadesSubmitBtn" 
-                    onClick={this.sendPushNotification}
-                >
-                    Send Notification
-                </button>
+
+                <div className="container addshifts-container" style={{
+                    position: 'absolute', left: '50%', top: '50%',
+                    transform: 'translate(-50%, -50%)'
+                }}>
+                    <div className="jumbotron" >
+
+                        <button 
+                            className="submit-btn" 
+                            id="decadesSubmitBtn" 
+                            onClick={this.sendPushNotification}
+                        >
+                            Send Notification
+                        </button>
+
+                        <Multiselect
+                            options={this.state.options} // Options to display in the dropdown
+                            onSelect={this.onSelect} // Function will trigger on select event
+                            onRemove={this.onRemove} // Function will trigger on remove event
+                            displayValue="name" // Property name to display in the dropdown options
+                            ref={this.multiselectRef}
+                        />
+                        
+
+                    </div>
+                </div>
 		    </div>
 		);
 	}
