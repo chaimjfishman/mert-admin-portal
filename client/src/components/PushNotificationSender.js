14153de6c666c1586b27c27d982f273845899bff
chaimjfishman <chaim.fishman2@gmail.com>
2021-01-28 13:03:45 -0500

    updated client to send member push tokens for notification

--- client/src/components/PushNotificationSender.js
+++ client/src/components/PushNotificationSender.js
@@ -13,17 +13,19 @@ export default class PushNotificationSender extends React.Component {
 		// State maintained by this React component
 		this.state = {
             allSelected: false,
-            members: [],
-            options: [{name: 'dfs', id: 1},{name: 'drw', id: 1},{name: 'd', id: 1},{name: 'a', id: 1},{name: 'b', id: 1},{name: 'c', id: 1},{name: 'Srigar', id: 1},{name: 'Sam', id: 2}]
+            options: [],
+            title: "",
+            message: ""
         }
 
         this.onSelect = this.onSelect.bind(this);
         this.onRemove = this.onRemove.bind(this);
         this.resetSelcted = this.resetSelcted.bind(this);
         this.sendPushNotification = this.sendPushNotification.bind(this);      
-        this.selectAllChange = this.selectAllChange.bind(this);      
+        this.selectAllChange = this.selectAllChange.bind(this);     
+        this.handleTitleChange = this.handleTitleChange.bind(this);     
+        this.handleMessageChange = this.handleMessageChange.bind(this);     
           
-
         this.multiselectRef = React.createRef();
     }
     
@@ -35,41 +37,52 @@ export default class PushNotificationSender extends React.Component {
           .then(res => res.json()) // Convert the response data to a JSON.
           .then(memberList => {
             if (!memberList) return;
-
-            // Map each memberObj in memberList to an HTML element:
-            // A button which triggers the showMovies function for each genre.
-            let memberDivs = memberList.map((memberObj, i) =>
-                <option value={memberObj.id}> {memberObj.email} </option>
-            );
     
             this.setState({
-                members: memberDivs
+                options: memberList
             })
           })
           .catch(err => console.log(err))	// Print the error if there is one.
     }
 
     sendPushNotification() {
-        console.log(`allSelected: ${this.state.allSelected}`)
-        console.log(this.multiselectRef.current.getSelectedItems())
-        console.log('options')
-        console.log(this.state.options)
+        let allRecipients = this.state.allSelected ? this.state.options : this.multiselectRef.current.getSelectedItems()
+        let pushTokens=''
+        allRecipients.forEach(recipient => pushTokens+=recipient.pushToken+',')
+
+        let url = `http://localhost:8081/notifications/${pushTokens}/${this.state.title}/${this.state.message}`
+        // Send an HTTP request to the server.
+        fetch(url, {
+                method: 'GET' // The type of HTTP request.
+            })
+            .then(res => res.json()) // Convert the response data to a JSON.
+            .then(resCode => {
+                console.log(resCode)
+            })
+            .catch(err => console.log(err))	// Print the error if there is one.
     }
 
-    onSelect(selectedList, selectedItem) {
-        console.log('selected')
+    handleTitleChange(e) {
+        this.setState({ 
+            title: e.target.value 
+        })
     }
+
+    handleMessageChange(e) {
+        this.setState({ 
+            message: e.target.value 
+        })
+    }
+
+    onSelect(selectedList, selectedItem) {}
     
-    onRemove(selectedList, removedItem) {
-        console.log('removed')
-    }
+    onRemove(selectedList, removedItem) {}
 
     resetSelcted() {
         this.multiselectRef.current.resetSelectedValues()
     }
 
     selectAllChange() {
-        console.log('changed')
         this.setState({
             allSelected: !this.state.allSelected
         })
@@ -86,14 +99,14 @@ export default class PushNotificationSender extends React.Component {
                 }}>
                     <div className="jumbotron" >
 
-                        <div class="form-check">
-                            <input class="form-check-input" 
+                        <div className="form-check">
+                            <input className="form-check-input" 
                                 type="checkbox" value="" 
                                 id="defaultCheck1"
                                 onClick={this.selectAllChange}
                             />
                             
-                            <label class="form-check-label">
+                            <label className="form-check-label">
                                 Notify Everyone!
                             </label>
                         </div>
@@ -102,7 +115,7 @@ export default class PushNotificationSender extends React.Component {
                             options={this.state.options} // Options to display in the dropdown
                             onSelect={this.onSelect} // Function will trigger on select event
                             onRemove={this.onRemove} // Function will trigger on remove event
-                            displayValue="name" // Property name to display in the dropdown options
+                            displayValue="email" // Property name to display in the dropdown options
                             ref={this.multiselectRef}
                             closeOnSelect={false}
                             disable={this.state.allSelected}
@@ -118,7 +131,29 @@ export default class PushNotificationSender extends React.Component {
                         </button>
 
                         <div>
-                            <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
+                            <br/><br/><br/>
+                        </div>
+
+                        <div class="input-group input-group-sm mb-3">
+                            <div class="input-group-prepend">
+                                <span class="input-group-text" id="inputGroup-sizing-sm">Title</span>
+                            </div>
+                            <input type="text" class="form-control" aria-label="Small" aria-describedby="inputGroup-sizing-sm" onChange={this.handleTitleChange}/>
+                        </div>
+
+                        <div>
+                            <br/>
+                        </div>
+
+                        <div class="input-group input-group-sm mb-3">
+                            <div class="input-group-prepend">
+                                <span class="input-group-text" id="inputGroup-sizing-sm">Message</span>
+                            </div>
+                            <input type="text" class="form-control" aria-label="Small" aria-describedby="inputGroup-sizing-sm" onChange={this.handleMessageChange}/>
+                        </div>
+
+                        <div>
+                            <br/><br/><br/><br/><br/><br/><br/><br/>
                         </div>
                         
                         <button 
